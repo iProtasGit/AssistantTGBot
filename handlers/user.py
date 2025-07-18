c3c48c105b01d39f3a2dab722129167bb7318ad6
iProtas <gerus.kz@gmail.com>
2025-07-18 23:26:52 +0300

    Reworked firstly start settings, reworked inline keyboards

--- handlers/user.py
+++ handlers/user.py
@@ -3,8 +3,7 @@ from aiogram import Router, F
 from aiogram.filters import Command, CommandStart, or_f
 from aiogram.types import Message, CallbackQuery
 
-from keyboards.start_keyboard import yes_help_kb, weather_yes_no_kb
-from keyboards.settings_inline_keyboard import settings_kb
+from keyboards.keyboard_utils import use_inline_keyboard
 from lexicon.lexicon_ru import user_ru
 
 router = Router()
@@ -12,48 +11,60 @@ logger = logging.getLogger('OnFuture')
 
 @router.message(CommandStart())
 async def process_start_command(message: Message, i18n: dict[str, str | dict]):
-    logger.info('Start command')
+    logger.debug('Start command started')
     await message.answer(
         text=i18n['commands']['start'], 
-        reply_markup=yes_help_kb)
+        reply_markup=use_inline_keyboard(2, 'who_i_am'))
     
     
-@router.message(or_f(Command(commands='help'), F.text == user_ru['buttons']['help']))
+@router.message(or_f(Command(commands='help')))
 async def process_help_command(message: Message, i18n):
-    logger.info('Help command')
+    logger.debug('Help command started')
     await message.answer(text=i18n['commands']['help'])
     
     
 @router.message(Command('settings'))
 async def process_settings(message: Message, i18n):
-    logger.info(f'Settings command {i18n["commands"]["settings"]}')
-    await message.answer(text=i18n['commands']['settings'], reply_markup=settings_kb)
+    logger.debug('Settings command started')
+    await message.answer(text=i18n['commands']['settings'], 
+                         reply_markup=use_inline_keyboard(2, 'settings'))
+
+@router.callback_query(F.data.in_(user_ru['inline_buttons']['who_i_am'].keys()))
+async def process_named(callback: CallbackQuery, i18n):
+    await callback.message.edit_text(
+        text=i18n['answers']['named'], 
+        reply_markup=use_inline_keyboard(2, 'weather'))
     
     
-@router.callback_query(
-    F.data == user_ru['inline_buttons']['turn_on_weather']['callback_data']
-    )
+@router.callback_query(F.data == 'turn_weather')
 async def process_turn_on_weather_button_click(callback: CallbackQuery, i18n):
     await callback.answer(text=i18n['answers']['turn_on_weather']) 
     
     
-@router.callback_query(
-    F.data == user_ru['inline_buttons']['turn_off_weather']['callback_data']
-    )
+@router.callback_query(F.data == 'turn_on_weather')
 async def process_turn_off_weather_button_click(callback: CallbackQuery, i18n):
     await callback.answer(text=i18n['answers']['turn_off_weather'])
      
      
-@router.message(F.text == user_ru['buttons']['yes'])
-async def process_yes_button(message: Message, i18n):
-    logger.info('Start yes button')
-    await message.answer(text=i18n['answers']['ready'], reply_markup=weather_yes_no_kb)
+@router.callback_query(F.data == 'yes_weather')
+async def process_yes_button(callback: CallbackQuery, i18n):
+    logger.debug('Start yes button')
+    await callback.message.edit_text(
+        text=i18n['answers']['ready'],
+        reply_markup=use_inline_keyboard(2, 'city_of_weather'))
+    
+
+@router.callback_query(
+    F.data.in_(user_ru['inline_buttons']['city_of_weather'].keys()))
+async def process_city_of_weather_button(callback: CallbackQuery, i18n):
+    logger.debug('City of weather selected')
+    await callback.message.answer(text=i18n['answers']['selected_city_of_weather'])
     
     
-@router.message(F.text == user_ru['buttons']['weather_no'])
-async def process_weather_no_button(message: Message, i18n):
-    logger.info('Weather no button')
-    await message.answer(text=i18n['answers']['no_weather'])
+@router.callback_query(F.data == 'no_weather')
+async def process_weather_no_button(callback: CallbackQuery, i18n):
+    logger.debug('Weather no button')
+    await callback.answer(text=i18n['answers']['no_weather'])
     
     
 @router.message()
--- keyboards/keyboard_utils.py
+++ keyboards/keyboard_utils.py
@@ -1,29 +1,63 @@
+from logging import getLogger
+
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from lexicon.lexicon_ru import inline_buttons
 
-def create_new_inline_kb(
+from lexicon.lexicon_ru import user_ru
+
+logger = getLogger('OnFuture')
+
+# def create_new_inline_kb(
+#     width: int,
+#     *args: str,
+#     **kwargs: str) -> InlineKeyboardMarkup:
+    
+#     kb_builder = InlineKeyboardBuilder()
+    
+#     buttons: list[InlineKeyboardButton] = []
+    
+#     if args:
+#         for button in args:
+#             buttons.append(InlineKeyboardButton(
+#                 text=inline_buttons[button] if button in inline_buttons else button,  # type: ignore
+#                 callback_data=button
+#         ))
+#     if kwargs:
+#         for button, text in kwargs.items():
+#             buttons.append(InlineKeyboardButton(
+#                 text=text,
+#                 callback_data=button
+#             ))
+    
+#     kb_builder.row(*buttons, width=width)
+    
+#     return kb_builder.as_markup() 
+
+def use_inline_keyboard(
     width: int,
-    *args: str,
-    **kwargs: str) -> InlineKeyboardMarkup:
+    name: str
+    ) -> InlineKeyboardMarkup:
     
     kb_builder = InlineKeyboardBuilder()
     
     buttons: list[InlineKeyboardButton] = []
     
-    if args:
-        for button in args:
+
+    
+    for button, text in user_ru['inline_buttons'][name].items():
+        if button[0:4] != 'url:':
             buttons.append(InlineKeyboardButton(
-                text=inline_buttons[button] if button in inline_buttons else button,  # type: ignore
-                callback_data=button
-        ))
-    if kwargs:
-        for button, text in kwargs.items():
+                text = text,
+                callback_data= button
+            ))
+        else:
             buttons.append(InlineKeyboardButton(
-                text=text,
-                callback_data=button
+                text = text,
+                url = button[4:]
             ))
     
     kb_builder.row(*buttons, width=width)
     
-    return kb_builder.as_markup() 
+    return kb_builder.as_markup()
+    
+    
